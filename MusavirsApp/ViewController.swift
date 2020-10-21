1c0400b5c4717ad31bb88e3b53bc703ae4b847d5
SyedMuneebUrRehman94 <muneeb.aimviz@gmail.com>
2020-10-21 14:38:12 +0500

    Initial Commit

---
+++ MusavirsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusavirsApp
+//
+//  Created by Syed Muneeb Ur Rehman on 21/10/2020.
+//  Copyright © 2020 Syed Muneeb Ur Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
