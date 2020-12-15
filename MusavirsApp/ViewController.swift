304d562f0befeab8419c6f89330c4e788caece19
SyedMuneebUrRehman94 <muneeb.aimviz@gmail.com>
2020-12-15 17:30:12 +0500

    initial

--- MusavirsApp/ViewController.swift
+++ MusavirsApp/ViewController.swift
@@ -9,12 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numberOnScreen:Double = 0;
 
+    
+
+ @IBOutlet weak var labels: UILabel!
+   
+    @IBAction func numbers(_ sender: UIButton) //numbers buttons
+    {
+     
+        labels.text = labels.text! + String(sender.tag-1) //
+        
+        numberOnScreen = Double(labels.text!)! //the answer will be decimal
+     }
+    
+    
+  @IBAction func button(_ sender: UIButton) // UI Buttons
+  {
+        
+        if labels.text != "" && sender.tag != 11 && sender.tag != 16 // show the buttons in UI
+        {
+            if sender.tag == 12 //Divide
+            {
+                labels.text = "÷";
+            }
+           else if sender.tag == 13 //Multiply
+            {
+                labels.text = "x";
+            }
+           else if sender.tag == 14 //subtract
+            {
+                labels.text = "-";
+            }
+            else if sender.tag == 15 //add
+            {
+                labels.text = "+";
+            }
+        }
+        
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         super.viewDidLoad()
+    
+     }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
-
 }
-
